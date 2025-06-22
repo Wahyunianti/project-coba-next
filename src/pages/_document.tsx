7b43aef1a3c4a80e78ba10564454e53df9e189bf
wahyunianti2002 <wahyunianti2002@gmail.com>
2025-06-22 19:43:39 +0700

    Add Landing Page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,28 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Title dan Deskripsi */}
+        <meta name="title" content="Promo Suzuki Alsut" />
+        <meta name="description" content="Kami menyediakan unit mobil suzuki terlengkap dengan harga terbaru dan pastinya promo yang menarik. Hanya di showroom kami Anda bisa dengan mudah mendapatkan mobil impian dengan DP yang ringan." />
+
+        {/* Open Graph (untuk Facebook, WhatsApp, dsb) */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.example.com/" />
+        <meta property="og:title" content="Promo Suzuki Alsut" />
+        <meta property="og:description" content="Kami menyediakan unit mobil suzuki terlengkap dengan harga terbaru dan pastinya promo yang menarik. Hanya di showroom kami Anda bisa dengan mudah mendapatkan mobil impian dengan DP yang ringan." />
+        <meta property="og:image" content="https://www.example.com/preview.jpg" />
+
+        {/* Twitter Card */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.example.com/" />
+        <meta property="og:title" content="Promo Suzuki Alsut" />
+        <meta property="og:description" content="Kami menyediakan unit mobil suzuki terlengkap dengan harga terbaru dan pastinya promo yang menarik. Hanya di showroom kami Anda bisa dengan mudah mendapatkan mobil impian dengan DP yang ringan." />
+        <meta property="og:image" content="https://www.example.com/preview.jpg" />
+
+        {/* Favicon (opsional) */}
+        <link rel="icon" href="/sujuki.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
