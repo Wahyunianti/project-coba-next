8aa82cf8aee7b66deb677c3f92f2ce4d19fc88fe
wahyunianti2002 <wahyunianti2002@gmail.com>
2025-06-22 19:48:14 +0700

    Add Landing Page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,17 +10,17 @@ export default function Document() {
 
         {/* Open Graph (untuk Facebook, WhatsApp, dsb) */}
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.example.com/" />
+        <meta property="og:url" content="https://promosuzukialsut.vercel.app/" />
         <meta property="og:title" content="Promo Suzuki Alsut" />
         <meta property="og:description" content="Kami menyediakan unit mobil suzuki terlengkap dengan harga terbaru dan pastinya promo yang menarik. Hanya di showroom kami Anda bisa dengan mudah mendapatkan mobil impian dengan DP yang ringan." />
-        <meta property="og:image" content="https://www.example.com/preview.jpg" />
+        <meta property="og:image" content="https://promosuzukialsut.vercel.app/suzuki-showroom.png" />
 
         {/* Twitter Card */}
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://www.example.com/" />
+        <meta property="twitter:url" content="https://promosuzukialsut.vercel.app/" />
         <meta property="og:title" content="Promo Suzuki Alsut" />
         <meta property="og:description" content="Kami menyediakan unit mobil suzuki terlengkap dengan harga terbaru dan pastinya promo yang menarik. Hanya di showroom kami Anda bisa dengan mudah mendapatkan mobil impian dengan DP yang ringan." />
-        <meta property="og:image" content="https://www.example.com/preview.jpg" />
+        <meta property="og:image" content="https://promosuzukialsut.vercel.app/suzuki-showroom.png" />
 
         {/* Favicon (opsional) */}
         <link rel="icon" href="/sujuki.png" />
